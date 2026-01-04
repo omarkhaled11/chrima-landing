8a67f9bd443acb79f75fbb3726a4f14df470db76
omarkhaled11 <omar.mohamed@gls-studio.com>
2026-01-04 23:15:06 +0100

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@vercel/analytics/astro';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vercel()],
+});
